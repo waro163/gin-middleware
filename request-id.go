3676069298eca526005b86b3db76cc52bcfe3cd5
Wang Rong <wangron@tesla.com>
2023-04-19 23:49:03 +0800

    [feat] add reqest-id

--- request-id.go
+++ request-id.go
@@ -1,7 +1,28 @@
 package ginmiddleware
 
-var headerXRequestID string
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
 
-// Config defines the config for RequestID middleware
-type config struct {
+var HeaderXRequestID string = "X-Request-ID"
+
+func AddRequestID() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var reqID string
+		if requestIDInHeader := c.GetHeader(HeaderXRequestID); requestIDInHeader != "" {
+			reqID = requestIDInHeader
+		} else {
+			reqID = uuid.New().String()
+			c.Request.Header.Set(HeaderXRequestID, reqID)
+		}
+
+		c.Set(HeaderXRequestID, reqID)
+		// set request id on request's context
+		c.Request = c.Request.WithContext(
+			WithMoreContextMeta(c.Request.Context(), ContextMetaData{Key: HeaderXRequestID, Value: reqID}),
+		)
+
+		c.Next()
+	}
 }
