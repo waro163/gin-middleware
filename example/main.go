f8fb2b82d6b11114e2b80e6bd53094ada6a3f91b
Wang Rong <wangron@tesla.com>
2023-04-27 18:51:20 +0800

    [doc] add example

---
+++ example/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	gmw "github.com/waro163/gin-middleware"
+)
+
+func main() {
+	r := gin.New()
+	writer := os.Stdout
+	log := &gmw.RequestLog{Output: writer}
+
+	r.Use(gmw.AddRequestID(), gmw.Recovery(writer), log.AddRequestLog())
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.POST("/panic", func(ctx *gin.Context) {
+		a := 0
+		b := 10 / a
+		c := []int{a, b}
+		ctx.JSON(200, c[99])
+		panic("error")
+	})
+
+	r.Run(":8081")
+}
