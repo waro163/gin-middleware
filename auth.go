bc7d947474309a18744515ea5507c722fec2693a
Wang Rong <wangron@tesla.com>
2025-02-23 16:49:33 +0800

    [feat] add NewAuthMiddleware function for auth

--- auth.go
+++ auth.go
@@ -8,6 +8,40 @@ import (
 
 var AuthPayload string = "auth-payload"
 
+func NewAuthMiddleware(getToken IgetAuthToken, authenticator IAuthenticator) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token, err := getToken.GetAuthToken(c.Request)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, &Error{
+				Code:    http.StatusUnauthorized,
+				Message: err.Error(),
+			})
+			return
+		}
+		if token == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, &Error{
+				Code:    http.StatusUnauthorized,
+				Message: "not found auth token",
+			})
+			return
+		}
+		claims, mErr := authenticator.Authenticate(token)
+		if mErr != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, &Error{
+				Code:    http.StatusUnauthorized,
+				Message: mErr.Message,
+			})
+			return
+		}
+		c.Set(AuthPayload, claims)
+		c.Next()
+	}
+}
+
+func GetAuthPayload(c *gin.Context) (interface{}, bool) {
+	return c.Get(AuthPayload)
+}
+
 type AuthMiddleware struct {
 	GetToken      IgetAuthToken
 	Authenticator IAuthenticator
